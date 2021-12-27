4833433576632ef9fdc28bb6a8a17cbb67751989
Galina Franchuk <galina20130509@gmail.com>
2021-12-27 16:09:17 +0200

    burger-menu

--- product_work/js/main.js
+++ product_work/js/main.js
@@ -1,3 +1,32 @@
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu__btn');
+  let links = menu.find('.burger-menu__link');
+  let overlay = menu.find('.burger-menu__overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('burger-menu__active');
+    
+    if (menu.hasClass('burger-menu__active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
+
+
+
 $(function(){
 	$('.strategies__content').slick({
     arrows: false,
